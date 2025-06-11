297177e4b7cf3a229a64c1d41eb4c1073b9ee499
Hieu Nguyen <hieutnguyen@kms-technology.com>
2025-06-11 16:56:41 +0700

    chore: add babel configuration, update jest config, modify junit.xml for test results, and enhance tsconfig

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   collectCoverageFrom: [
     "(app|components|hooks|stores|lib|providers)/**/*.{ts,tsx}",
   ],
+  transformIgnorePatterns: [
+    // Đây là phần quan trọng: loại trừ các thư viện này khỏi việc bị bỏ qua transform
+  ],
   collectCoverage: false,
   coverageThreshold: {
     global: {
@@ -14,3 +17,14 @@ module.exports = {
     },
   },
 };
+
+// // jest.config.js
+// module.exports = {
+//   preset: 'react-native',
+//   // ... các cấu hình khác của bạn ...
+//   transformIgnorePatterns: [
+//     // Đây là phần quan trọng: loại trừ các thư viện này khỏi việc bị bỏ qua transform
+//     'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|expo(nent)?|@expo(nent)?/.*|sentry-expo|native-base|react-native-svg|react-native-reanimated|react-native-safe-area-context|@gorhom/bottom-sheet|react-native-video|expo-linear-gradient|lucide-react-native)',
+//   ],
+//   // ... các cấu hình khác ...
+// };
